c1e3e918d93bb30bd0bc94ab7bd4607b393d5f5e
Pattarakarn <pattarakarn@outlook.com>
2024-03-01 21:52:50 +0700

    test

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    let content = ""
+
+    try {
+        const filePath = path.resolve('population-and-demography.csv')
+
+        let file = fs.readFileSync(filePath, { encoding: 'utf-8' }, function (err) {
+            console.log(err);
+        });
+
+        file = file.split("\n");
+
+        headers = file.shift().split(",");
+
+        const json = [];
+        file.forEach(function (data) {
+
+            row = {}
+            arrData = data.split(",")
+
+            for (let i = 0; i < headers.length; i++) {
+                row[headers[i]] = arrData[i];
+            }
+
+            json.push(row);
+        });
+
+        content = JSON.stringify(json)
+
+    } catch (error) {
+        console.error(error)
+    }
+
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    response.send(content)
+});
+
+const port = 8000
+app.listen(port, () => {
+    console.log(`server running at port ${port}`)
+})
